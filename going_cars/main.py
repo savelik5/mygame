174b5210be7e52e7c4c996ed06de78aeb3d8399d
savelik5 <savelik0409@gmail.com>
2024-01-27 12:00:25 +0300

    Добавили новый игровой цикл Co-authored-by: Egor Smyshnikov <Smyshnikof@users.noreply.github.com>

--- going_cars/main.py
+++ going_cars/main.py
@@ -2,38 +2,47 @@ import random
 import time
 import pygame
 
+pygame.init()
+
 BLACK = pygame.Color(0, 0, 0)
 WHITE = pygame.Color(255, 255, 255)
-
-pygame.init()
+FPS = 120
 
 screen_width = 800
 screen_height = 800
 screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("going cars")
 
 car_width = 80
 car_height = 100
 
 #car, background, lines_on_road, intro_background
-car = pygame.image.load("Asphalt-027.jpg")
+# car = pygame.image.load("asphalt-027.jpg")
 
-pygame.display.set_caption("going cars")
 
-FPS = 120  
 
 clock = pygame.time.Clock()
 paused = False
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    
-    
+
+
+def intro():
+    print("Это меню игры")
+
+
+def game_loop():
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
         
-    pygame.display.update()
+        
 
-    clock.tick(FPS)
+        pygame.display.update()
 
+        clock.tick(FPS)
 
+intro()
+game_loop()
 pygame.quit() 
+
